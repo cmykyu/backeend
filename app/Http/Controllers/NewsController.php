23b62d974ac22ca7d15f7f0733594d6581e433ae
yushanlife0904 <61447553+yushanlife0904@users.noreply.github.com>
2020-03-06 07:08:07 +0800

    update_03/05_2

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -22,11 +22,14 @@ class NewsController extends Controller
 
     public function store (Request $request){
         $news_data = $request->all();
+        //可先dd
+        // dd($news_data);
 
         // //上傳檔案
         // $file_name = $request->file('img')->store('','public');
         // $news_data['img'] = $file_name;
 
+        //上傳主要圖片
         if($request->hasFile('img')) {
             $file = $request->file('img');
             $path = $this->fileUpload($file,'news');
@@ -36,48 +39,77 @@ class NewsController extends Controller
 
         $new_news = News::create($news_data);
 
-        //多張圖片上傳
+            //多張圖片上傳
 
-        if($request->hasFile('news_imgs')){
+            if($request->hasFile('news_imgs')){
 
-        $files = $request->file('news_imgs');
+            $files = $request->file('news_imgs');
 
-        foreach ($files as $file) {
-        //上傳圖片
-        $path = $this->fileUpload($file,'news');
+            foreach ($files as $file) {
+                //上傳圖片
+                $path = $this->fileUpload($file,'news');
 
-        //新增資料進DB
+                //新增資料進DB
 
-        $news_imgs = new NewsImgs;
-        $news_imgs->news_id = $new_news->id;
-        $news_imgs->img_url = $path;
-        $news_imgs->save();
+                $news_imgs = new NewsImgs;
+                $news_imgs->news_id = $new_news->id;
+                $news_imgs->img_url = $path;
+                $news_imgs->save();
 
+                }
             }
-        }
 
 
 
-        //ID位置要一致
-        News::create ($news_data);
-        return redirect('/home/news');
+            //ID位置要一致
+            // News::create ($news_data);
+            return redirect('/home/news');
     }
 
     public function edit ($id){
 
-        $news = News::where('id','=',$id)->first();
+        // $news = News::where('id','=',$id)->first();
+        $news = News::with("news_imgs")->find($id);
         return view('admin/news/edit',compact('news'));
     }
 
     public function update (Request $request,$id){
-        News::find($id)->update($request->all());
-        return redirect('/home/news');
 
-    }
-    public function delete (Request $request,$id){
-        News::find($id)->delete();
+        $request_data = $request->all();
+        $item = News::find($id);
+
+        //假如上傳新圖片
+        if($request->hasFile('img')){
+
+            //舊圖片就要刪除
+            $old_image = $item->img;
+            File::delete(public_path().$old_image);
+
+            //再上傳新圖片
+            $file = $request->file('img');
+            $path = $this->fileUpload($file,'news');
+            $request_data['img'] = $path;
+        }
+        $item->update($request_data);
         return redirect('/home/news');
     }
+
+    public function delete (Request $request,$id){
+
+        $item = News::find($id);
+
+        $old_image = $item->img;
+
+        //下判斷式
+        if(file_exists(public_path().$old_image)){
+            File::delete(public_path().$old_img);
+
+        }
+
+        $item->delete();
+        return redirect('/home/news');
+    }
+
     private function fileUpload($file,$dir){
         //防呆：資料夾不存在時將會自動建立資料夾，避免錯誤
         if( ! is_dir('upload/')){
@@ -95,5 +127,21 @@ class NewsController extends Controller
         move_uploaded_file($file, public_path().'/upload/'.$dir.'/'.$filename);
         //回傳 資料庫儲存用的路徑格式
         return '/upload/'.$dir.'/'.$filename;
+    }
+    public function ajax_delete_news_imgs(Request $request)
+    {
+        $newsimgid = $request->newsimgid;
+
+        $item = NewsImgs::find($newsimgid);
+        $old_image = $item->img;
+
+        if(file_exists(public_path().$old_image)){
+            File::delete(public_path().$old_image);
+        }
+
+        $item->delete();
+
+
+        return "delete success";
     }
 }
--- resources/views/front/news_detail.blade.php
+++ resources/views/front/news_detail.blade.php
@@ -5,22 +5,22 @@
 @endsection --}}
 
 @section('content')
-<section class="engine"><a href="https://mobirise.info/x">css templates</a></section><section class="features3 cid-rRF3umTBWU" id="features3-7" style="padding:100px">
-<div class="container">
-    <div class="media-container-row">
-        {{$news}}
-
-        @foreach ($news->news_imgs as $news_img)
-        {{-- {{$news_img->img_url}} --}}
-        <img src="{{asset('/storage/'.$news->img)}}" alt="" width="250">
-        @endforeach
+<section class="engine">
+    <a href="https://mobirise.info/x">css templates</a></section><section class="features3 cid-rRF3umTBWU" id="features3-7" style="padding:100px">
+    <div class="container">
+        <div class="media-container-row">
+            title : {{$news->title}}
+            <br>
+            多張圖片:
+            @foreach ($news->news_imgs as $news_img)
+            {{-- {{$news_img->img_url}} --}}
+            <img src="{{asset('/storage/'.$news->img)}}" alt="" width="250">
+            @endforeach
+        </div>
 
     </div>
 
-
-
-</div>
-
 @endsection
+
 
 
